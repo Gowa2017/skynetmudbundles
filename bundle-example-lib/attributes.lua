b5d3a913faae1959a2c44727489c02fc6b0e4c43
Gowa2017 <33367355+Gowa2017@users.noreply.github.com>
2021-10-22 23:16:42 +0800

    make mana and favor in class bundle

--- bundle-example-lib/attributes.lua
+++ bundle-example-lib/attributes.lua
@@ -1,16 +1,5 @@
 return {
   {
-    name    = "mana",
-    base    = 100,
-    formula = {
-      requires = { "intellect" },
-      fn       = function(character, mana, intellect)
-        return mana + (intellect * 10);
-      end,
-    },
-  },
-  { name = "favor", base = 10 },
-  {
     name    = "health",
     base    = 100,
     formula = {
